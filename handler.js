ae0314e6ab774e7b2104299ebceed8674bb5070c
Wandygans <87267241+Wandygans@users.noreply.github.com>
2022-12-11 19:32:16 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -31,18 +31,17 @@ module.exports = {
         global.img = 'https://telegra.ph/file/dffdaf0e8d59ad104e526.jpg'
         global.fla = pickRandom(global.flaaa)
         global.namabot = conn.user.name
-        global.packname = '© RaaBotz-MD'
-        global.author = 'Made By Claraa © 2022'
+        global.packname = '© Zero-Bot'
+        global.author = 'Made By Wandy © 2022'
         global.packname2 = 'Made With'
-        global.author2 = 'Raa Bot By Claraa © 2022'
-        global.wm3 = '·͟͟͟͟͟͟͞͞͞͞͞͞Made By Claraa˚'
-        global.wm2 = '©𝙍𝙖𝙖-𝘽𝙤𝙩𝙯-𝙈𝘿' + ' ' + 'By 𝐂𝐥𝐚𝐫𝐚𝐚'
-        global.wm = '© 𝙍𝙖𝙖-𝘽𝙤𝙩𝙯--𝙈𝘿'
+        global.author2 = 'Zero Bot By Wandy © 2022'
+        global.wm3 = '·͟͟͟͟͟͟͞͞͞͞͞͞Made By Wandy˚'
+        global.wm2 = 'ZeroBot'
+        global.wm = '©ZeroBot'
         global.colong1 = 'Ciss 📸'
-        global.colong2 = 'By Claraa'
+        global.colong2 = 'By Wandy'
         global.kontak2 = [
-        ['62895604670507', 'Claraa', 'Creator Bot', 'Clarz939@support.com', true],
-        ['79217628068', 'Raa Botz-MD', 'Bot WhatsApp', 'Just Bot', true]
+        ['6282125039170', 'Wandygans', 'Bot WhatsApp', 'Just Bot', true]
         ]
         global.bg = await (await fetch(img)).buffer()
         global.time = require('moment-timezone').tz('Asia/Jakarta').format('HH:mm:ss')
@@ -499,7 +498,7 @@ module.exports = {
                         continue
                     }
                     if (plugin.register == true && _user.registered == false) { // Need register?
-                        fail('unreg', m, this)
+                        fail('unreg')
                         continue
                     }
                     m.isCommand = true
