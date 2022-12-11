de03cd65cc7866e342ea7e5e1bff3ea890709480
Wandygans <87267241+Wandygans@users.noreply.github.com>
2022-12-11 19:23:40 +0700

    Add files via upload

---
+++ plugins/owner-exec.js
@@ -0,0 +1,35 @@
+let handler  = async (m, _2) => {
+budy = (m.mtype === 'conversation') ? m.message.conversation : (m.mtype == 'imageMessage') ? m.message.imageMessage.caption : (m.mtype == 'videoMessage') ? m.message.videoMessage.caption : (m.mtype == 'extendedTextMessage') ? m.message.extendedTextMessage.text : (m.mtype == 'buttonsResponseMessage') ? m.message.buttonsResponseMessage.selectedButtonId : (m.mtype == 'listResponseMessage') ? m.message.listResponseMessage.singleSelectReply.selectedRowId : (m.mtype == 'templateButtonReplyMessage') ? m.message.templateButtonReplyMessage.selectedId : (m.mtype === 'messageContextInfo') ? (m.message.buttonsResponseMessage?.selectedButtonId || m.message.listResponseMessage?.singleSelectReply.selectedRowId || m.text) : ''
+try {
+let evaled = await eval(budy.slice(2))
+if (typeof evaled !== 'string') evaled = require('util').inspect(evaled)
+await m.reply(evaled)
+} catch (err) {
+await m.reply(String(err))
+}
+}
+
+handler.help = ['> ', '=> ']
+handler.tags = ['advanced']
+handler.customPrefix = /^=?> /
+handler.command = /(?:)/i
+handler.rowner = true
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
+
+class CustomArray extends Array {
+  constructor(...args) {
+    if (typeof args[0] == 'number') return super(Math.min(args[0], 10000))
+    else return super(...args)
+  }
+}
---
+++ plugins/owner.js
@@ -0,0 +1,14 @@
+async function handler(m) {
+let vcard = 'BEGIN:VCARD\n'
++ 'VERSION:3.0\n' 
++ 'N:;Wandy;;;'
++ 'FN:ZEROSTORE\n'
++ 'ORG:OwnerBot;\n'
++ 'TEL;type=CELL;type=VOICE;waid=6282125039170:+62 821-2503-9170\n' // WhatsApp ID + phone number
++ 'END:VCARD'
+conn.sendMessage(m.chat, { contacts: { displayName: 'Wandy Gunawan', contacts: [{ vcard }] } }, { quoted: m })
+}
+
+handler.command = /^(owner|creator)$/i
+
+module.exports = handler
