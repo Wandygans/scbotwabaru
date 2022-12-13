a5809e3f6f245cbaaeae3596684cb2bbd088b745
Wandygans <87267241+Wandygans@users.noreply.github.com>
2022-12-13 18:10:19 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,48 +1,55 @@
-const axios = require('axios')
-const cheerio = require('cheerio')
-
-let handler = async(m, {conn, command, usedPrefix, text}) => {
-if (!text) throw `Example: ${usedPrefix + command} link tiktok`
-m.reply(wait)
-
-async function tiktok(url){
-    try {
-        const tokenn = await axios.get("https://downvideo.quora-wiki.com/tiktok-video-downloader#url=" + url);
-        let a = cheerio.load(tokenn.data);
-        let token = a("#token").attr("value");
-        const param = {
-            url: url,
-            token: token,
-        };
-        const { data } = await axios.request("https://downvideo.quora-wiki.com/system/action.php", {
-                method: "post",
-                data: new URLSearchParams(Object.entries(param)),
-                headers: {
-                    "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-                    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36",
-                    "referer": "https://downvideo.quora-wiki.com/tiktok-video-downloader",
-                },
-            }
-        );
-        return {
-            status: 200,
-            author: "WandyGans",
-            title: data.title,
-            thumbnail: "https:" + data.thumbnail,
-            duration: data.duration,
-            media: data.medias,
-        };
-    } catch (e) {
-        return e
-    }
+const axios = require("axios");
+const cheerio = require("cheerio");
+const clean = (data) => {
+	let regex = /(<([^>]+)>)/ig
+	data = data.replace(/(<br?\s?\/>)/ig, ' \n')
+	return data.replace(regex, '')
 }
 
-result = await tiktok(text)
-result = result.media[1].url;
-conn.sendMessage(m.chat, { video: result, caption: "", quoted: m})
+global.ftroli = { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net' }, message: { orderMessage: { itemCount: 9999999999999999999999999999999999999999999999999999999, status: 1, surface: 1, message: wm, orderTitle: wm, sellerJid: '0@s.whatsapp.net' } } }
+
+async function tiktok(query) {
+   return new Promise((resolve, reject) => {
+     axios("https://lovetik.com/api/ajax/search", {
+       method: "POST",
+       data: new URLSearchParams(Object.entries({ query }))
+     })
+     .then(({ data }) => {
+     if (!data.vid) {
+        resolve(data)
+        return !0
+     }
+        resolve({
+        desc: clean(data.desc),
+        uploader: clean(data.author),
+        thumbnail: data.cover,
+        medias: {
+          nowm: {
+             size: data.links[0].s || '',
+             url: data.links[0].a || ''
+                  },
+          watermark: {
+             size: data.links[1].s || '',
+             url: data.links[1].a || ''
+                       },
+          audio: {
+             sound: clean(data.links[2].s || ''),
+             url: data.links[2].a || ''
+                  }
+                 }
+        })
+      })
+   })
+}
+
+let handler = async(m,{text, conn}) => {
+m.reply(global.wait)
+url = await tiktok(text)
+conn.sendFile(m.chat, url.medias.nowm.url, null, ``, global.ftroli)
 }
 
 handler.command = ['tiktok']
-handler.exp = 3
+
+handler.exp = 10
 
 module.exports = handler
